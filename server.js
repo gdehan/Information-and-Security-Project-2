a661129821fc3a266b3c58343c65ef4d7d337982
Glitch (flying-tidal-raver) <none>
2024-03-26 19:06:09 +0000

    🏏🍦 Checkpoint ./server.js:65965251/231

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+
+let bcrypt = require('bcrypt')
+
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
+});
+
+
+
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
